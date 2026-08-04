4fd5e55089ccf6ec7bed2d4dc4681c37e9a6a335
gollymashed <gm@wassie.games>
2026-08-04 13:22:02 +0100

    initial commit

---
+++ src/main/java/com/studiomashed/threebyone/ThreebyoneApplication.java
@@ -0,0 +1,13 @@
+package com.studiomashed.threebyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreebyoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreebyoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studiomashed/threebyone/ThreebyoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiomashed.threebyone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreebyoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
